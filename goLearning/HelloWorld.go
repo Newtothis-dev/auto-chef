0f554400121148ff93eea7f1c5df79fb04ed98ef
pdev <pdev@NewToThiss-MacBook-Pro.local>
2020-11-04 06:51:06 -0500

    Added notes for 1-5 of pluralsight class

--- goLearning/HelloWorld.go
+++ goLearning/HelloWorld.go
@@ -1,8 +1,133 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func learnVar() {
+	// `var` declares 1 or more variables.
+	var a = "initial"
+	const aa = "initial const"
+	fmt.Println(aa)
+	fmt.Println(a)
+
+	// You can declare multiple variables at once.
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	// Go will infer the type of initialized variables.
+	var d = true
+	fmt.Println(d)
+
+	// Variables declared without a corresponding
+	// initialization are _zero-valued_. For example, the
+	// zero value for an `int` is `0`.
+	var e int
+	fmt.Println(e)
+
+	// The `:=` syntax is shorthand for declaring and
+	// initializing a variable, e.g. for
+	// `var f string = "apple"` in this case.
+	f := "apple"
+	fmt.Println(f)
+}
+
+func learnFor() {
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// A classic initial/condition/after `for` loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	// `for` without a condition will loop repeatedly
+	// until you `break` out of the loop or `return` from
+	// the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	// You can also `continue` to the next iteration of
+	// the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
+
+func learnSwitch() {
+	// Here's a basic `switch`.
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// You can use commas to separate multiple expressions
+	// in the same `case` statement. We use the optional
+	// `default` case in this example as well.
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// `switch` without an expression is an alternate way
+	// to express if/else logic. Here we also show how the
+	// `case` expressions can be non-constants.
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// A type `switch` compares types instead of values.  You
+	// can use this to discover the type of an interface
+	// value.  In this example, the variable `t` will have the
+	// type corresponding to its clause.
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
+
+func split() {
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("NEXT!")
+	fmt.Println("")
+	fmt.Println("")
+}
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("goodnight")
+
+	learnVar()
+	split()
+	learnFor()
 }
